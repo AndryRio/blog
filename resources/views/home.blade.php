3658f741b7e6fccef7c77fdb36fcf67913d47143
AndryRio <andreisprincenatu2@gmail.com>
2024-03-14 17:30:11 +0100

    differents route tests

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Home Dashboard</title>
+</head>
+<body>
+    <div>
+        {{ $name }} {{ $id }} <small>({{ $email }})</small>
+    </div>
+</body>
+</html>
